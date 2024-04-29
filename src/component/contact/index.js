87ecf21a7f7eeb0a2da0e769222621a2c3e1df8c
Natalyasor/SN <natalyasor110983@gmail.com>
2024-04-29 19:44:34 +0200

    react-airbnb

--- src/component/contact/index.js
+++ src/component/contact/index.js
@@ -1,20 +1,24 @@
 
 
 import "./index.css";
-import  photoKerthy from "./photoKerthy.svg"
+import photoKerthy from "./photoKerthy.svg";
 
 
 export default function Contact({name, image, response_rate, response_time, info, phone}) {
     return (
         <div>
-           <h1 className="contact__title">Господар – Kerthy</h1> 
-           <li className="contact__block">
-            <imageSrc photoKerthy ={photoKerthy} />
+          <div className="photoblock">
+          <img src ={photoKerthy} alt="photoKerthy"/>
+
+       <div className="contact__block">
+       <h1 className="contact__title">Господар – Kerthy</h1> 
             <span className="contact__value">{phone}</span>
-            <span className="contact__value">{response_time}</span>
-            <span className="contact__value">{response_rate}% відсотків швидкості відгуку</span>
-           </li>
-           <div className="contact__value">{info}</div>
+          <span className="contact__value">{response_time}</span>
+          <span className="contact__value">{response_rate}% відсотків швидкості відгуку</span>
+       </div>
+         
+          </div>
+            <div className="contact__value1">{info}</div>
             </div>
     )
 }
